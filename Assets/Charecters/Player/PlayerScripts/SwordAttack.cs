98d902f395f872a71545380875027418b51674b1
Alex <alex@gmsmatrix.com>
2023-10-17 22:27:21 -0500

    added SwordAttack

--- Assets/Charecters/Player/PlayerScripts/SwordAttack.cs
+++ Assets/Charecters/Player/PlayerScripts/SwordAttack.cs
@@ -5,38 +5,38 @@ using UnityEngine;
 public class SwordAttack : MonoBehaviour
 {
 
-    Collider2D swordCollider;
-    Vector2 attackOffset;
+    // Collider2D swordCollider;
+    // Vector2 attackOffset;
 
-    void Start()
-    {
-        swordCollider = GetComponent<Collider2D>();
-        attackOffset = transform.position;
-    }
+    // void Start()
+    // {
+    //     swordCollider = GetComponent<Collider2D>();
+    //     attackOffset = transform.position;
+    // }
 
-    public void attack(){
-        
-    }
+    // // public void attack(){
+
+    // // }
      
 
-    public void attackRight(){
-        swordCollider.enabled = true;
-        transform.position = rightAttackOffset;
+    // public void attackRight(){
+    //     swordCollider.enabled = true;
+    //     transform.position = rightAttackOffset;
 
-    }
+    // }
 
-    public void attackLeft() {
-        transform.position = new Vector2(attackOffset.x * -1, attackOffset.y);
-    }
-
-
+    // public void attackLeft() {
+    //     transform.position = new Vector2(attackOffset.x * -1, attackOffset.y);
+    // }
 
 
-    // Start is called before the first frame update
 
-    // Update is called once per frame
-    void Update()
-    {
+
+    // // Start is called before the first frame update
+
+    // // Update is called once per frame
+    // void Update()
+    // {
         
-    }
+    // }
 }
